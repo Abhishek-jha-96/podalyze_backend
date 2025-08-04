5b00c53826febfe6b7ffb408fc22581a76c0708f
Abhishek <abhishekjha96500@gmail.com>
2025-08-04 23:28:01 +0530

    cors config.

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import validationOptions from './utils/validation-options';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [process.env.ALLOWED_ORIGINS],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe(validationOptions));
 
