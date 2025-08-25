b9eba0b0a56e9b85213efaadb0384765206dfdbd
Abhishek <abhishekjha96500@gmail.com>
2025-08-25 22:32:39 +0530

    updated the script for running for build time.

--- run_server.sh
+++ run_server.sh
@@ -4,8 +4,8 @@
 if [ $# -ge 1 ]; then
   env=$1
   podman compose -f docker-compose.$env.yaml down
-  podman compose -f docker-compose.$env.yaml up -d
+  podman compose -f docker-compose.$env.yaml up -d --build
 else
   podman compose --env-file .env down
-  podman compose --env-file .env up -d
+  podman compose --env-file .env up -d --build
 fi
